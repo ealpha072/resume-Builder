9ce6eb9d0027eadfb88de0feca3bc1d79f6c0178
ealpha072 <ealpha072@gmail.com>
2022-11-06 14:56:08 +0300

    Initial folder structure

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <nav className='header-container'>
+            <div>
+                <div className="logo"></div>
+            </div>
+            <div>
+                <a href="">Login</a>
+            </div>
+            <div>
+                <a href="">Sign-up</a>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/header'
+
+const Login = () => {
+  return (
+    <Header />
+  )
+}
+
+export default Login
