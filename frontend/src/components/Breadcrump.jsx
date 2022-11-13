501d0ef30ba66d6ba1bae59390d2dab461318ad4
ealpha072 <ealpha072@gmail.com>
2022-11-13 17:11:24 +0300

    Breadcrump elements

---
+++ frontend/src/components/Breadcrump.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Breadcrump = () => {
+    let array_itmes = ['Information', 'Education', 'Work Experience', 'Key Skills', 'Profile', 'Complete']
+
+    return (
+        <div className='bredacrump-container'>
+            <div className="breadcrumps">
+                {array_itmes.map(item => (
+                    <div className="breadcrump-item" key={item}>
+                        <Link to='' > {item} </Link>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Breadcrump
--- frontend/src/pages/Editor.jsx
+++ frontend/src/pages/Editor.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import Contact from '../components/Contact'
+import Breadcrump from '../components/Breadcrump'
 
 const Editor = () => {
-  return (
-    <>
-        <div className='mainWrapper'>
-            <div className="mainWrapper-left"></div>
-            <div className="mainWrapper-right">
-                <Contact />
+    return (
+        <>
+            <div className='mainWrapper'>
+                <div className="mainWrapper-left"></div>
+                <div className="mainWrapper-right">
+                    <Contact />
+                </div>
             </div>
-        </div>
-        <div className="divider"></div>
-    </>
-  )
+            <div className="divider"></div>
+            <Breadcrump />
+            <div className="save-cont">
+                <div className="back-create-resume save-changes">
+                    <button className="back">
+                        Save
+                    </button>
+                </div>
+                <div className="back-create-resume">
+                    <button className="continue">
+                        Continue
+                    </button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Editor
