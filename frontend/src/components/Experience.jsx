963c7328dd92b80125f4cab7beba0236e5b53827
ealpha072 <ealpha072@gmail.com>
2023-05-15 22:01:20 +0300

    Experience page

---
+++ frontend/src/components/Experience.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "../styles/Experience.module.css"
+
+const Experience = () => {
+    <div className="information">
+        <div className="home-header">
+            <h1 className="header-head">Recent Professional Experience</h1>
+            <p className='color-p'>
+                Add your most recent job and continue in descending order.
+            </p>
+        </div>
+
+
+    </div>
+}
+
+export default Experience
